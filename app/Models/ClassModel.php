37ad6a127ecab8dfc541da942c30b8a105409f35
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2024-07-24 20:32:44 -0300

    [FTR] - :sparkles: Adiciona informações na tabela de turmas

--- app/Models/ClassModel.php
+++ app/Models/ClassModel.php
@@ -2,23 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ClassModel extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['name', 'school_id', 'teacher_id'];
+    protected $fillable = [
+        'name',
+        'shift',
+        'school_id',
+        'teacher_id'
+    ];
 
     protected $table = 'class';
 
-    public function user() {
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function teacher() {
+    public function teacher(): BelongsTo
+    {
         return $this->belongsTo(Teacher::class, 'teacher_id');
     }
 
