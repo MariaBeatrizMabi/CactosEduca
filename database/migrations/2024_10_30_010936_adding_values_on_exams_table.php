910f2b7055f4081e31283ff6d3a066ff2ca8e468
maurelio <marcos.aurelio@3esolucoes.com.br>
2024-10-29 22:11:22 -0300

    [WIP] - Criando migration para adicionar valores ao campo de leitura e escrita

---
+++ database/migrations/2024_10_30_010936_adding_values_on_exams_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('exams', function (Blueprint $table) {
+            $table->enum('reading', [
+                'not_reader',
+                'syllable_reader',
+                'word_reader',
+                'sentence_reader',
+                'fluent_text_reader',
+                'no_fluent_text_reader',
+                'missed',
+                'transferred'
+            ])->default('not_reader');
+            $table->enum('writing', [
+                'pre_syllabic',
+                'syllabic',
+                'alphabetical_syllabic',
+                'alphabetical',
+                'missed',
+                'transferred'
+            ])->default('pre_syllabic');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
