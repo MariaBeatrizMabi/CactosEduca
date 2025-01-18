f9f4236058ac3c9cf3b7299ce6ef9ec480ae3247
maurelio <marcos.aurelio@3esolucoes.com.br>
2025-01-18 18:35:08 -0300

    [FTR] - Validando erros no formulario de criação de sondagem na tela de detalhes da turma

--- resources/js/pages/ClassDetails.vue
+++ resources/js/pages/ClassDetails.vue
@@ -36,9 +36,13 @@ const createExamData = ref({
     writing: '',
     action: null
 });
+
+const hasErrors = ref({
+    reading: false,
+    writing: false
+})
 const literacyParameters = ref({});
 const selectedLiteracyParameters = ref([]);
-
 const selectedStudentToAdd = ref('');
 
 const classData = ref({
@@ -47,6 +51,23 @@ const classData = ref({
     shift: '',
     teacher_id: null
 });
+const clearErrors = () => {
+    hasErrors.value = {
+        reading: false,
+        writing: false
+    }
+}
+const validateData = () => {
+    clearErrors()
+
+    if(!createExamData.value.reading || createExamData.value.reading === ''){
+        hasErrors.value.reading = true
+    }
+
+    if(!createExamData.value.writing || createExamData.value.writing === ''){
+        hasErrors.value.writing = true
+    }
+}
 
 const formData = ref({
     name: '',
@@ -176,6 +197,13 @@ async function handleImportData() {
 }
 
 const openCreateExamModal = (studentId) => {
+    createExamData.value = {
+        reading: '',
+        writing: '',
+        action: null
+    };
+
+    clearErrors();
     studentIdToCreateExam.value = studentId
     openedCreateExamModal.value = true
 }
@@ -207,45 +235,43 @@ async function getStudentExams(studentId) {
 }
 
 const submitExamCreated = async () => {
-    const studentExams = await getStudentExams(studentIdToCreateExam.value);
-    isLoading.value = true
+    validateData();
 
-    try {
+    if(!(hasErrors.value.reading || hasErrors.value.writing)){
+        const studentExams = await getStudentExams(studentIdToCreateExam.value);
+        isLoading.value = true
 
-        const createdPoll = await axios.post(`/PollCreate`, {
-            name: studentExams.length + 1 + '° Período de sondagem',
-            class_id: classData.value.id,
-            poll_number: studentExams.length + 1,
-            school_id: school.value.id,
-            year: classData.value.id
-        });
+        try {
 
-        await api.post(`/api/exams`, {
-            ...createExamData.value,
-            student_id: studentIdToCreateExam.value,
-            class_id: classData.value.id,
-            poll_id: createdPoll.data.id,
-            literacy_parameters_values: selectedLiteracyParameters.value
-        });
+            const createdPoll = await axios.post(`/PollCreate`, {
+                name: studentExams.length + 1 + '° Período de sondagem',
+                class_id: classData.value.id,
+                poll_number: studentExams.length + 1,
+                school_id: school.value.id,
+                year: classData.value.id
+            });
 
-        createExamData.value = {
-            reading: '',
-            writing: '',
-            action: null
+            await api.post(`/api/exams`, {
+                ...createExamData.value,
+                student_id: studentIdToCreateExam.value,
+                class_id: classData.value.id,
+                poll_id: createdPoll.data.id,
+                literacy_parameters_values: selectedLiteracyParameters.value
+            });
+
+            createExamData.value = {
+                reading: '',
+                writing: '',
+                action: null
+            }
+
+        } catch (error) {
+            console.error("Erro ao criar a pesquisa:", error);
         }
-
-    } catch (error) {
-        console.error("Erro ao criar a pesquisa:", error);
+        openedCreateExamModal.value = false;
+        isLoading.value = false;
     }
-    openedCreateExamModal.value = false;
-    isLoading.value = false;
 
-    // console.log(studentExams.length)
-    // console.log(classData.value)
-    // console.log(school.value.id)
-    // console.log(createExamData.value)
-    // console.log(selectedLiteracyParameters.value)
-    // console.log(studentIdToCreateExam.value)
 }
 
 
@@ -265,43 +291,53 @@ const submitExamCreated = async () => {
             <a href="/documentos/instrucoesLeitura.pdf" target="_blank">Mais informações sobre ações de intervenção - Leitura</a>
 
             <div class="modal-content-details">
-                <SelectComponent
-                    labelTitle="Nível de leitura"
-                    placeholderValue="Nível de leitura"
-                    icon="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l293.1 0c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1l-91.4 0zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"
-                    typeValue="select"
-                    :value="createExamData.reading"
-                    valueField="id"
-                    RightAction="display: none;"
-                    @input="createExamData.reading = $event.target.value"
-                >
-                    <option value="not_reader">Não leitor</option>
-                    <option value="syllable_reader">Leitor de sílabas</option>
-                    <option value="word_reader">Leitor de palavras</option>
-                    <option value="sentence_reader">Leitor de frases</option>
-                    <option value="no_fluent_text_reader">Leitor de texto sem fluência</option>
-                    <option value="fluent_text_reader">Leitor de texto com fluência</option>
-                    <option value="missed">Faltou</option>
-                    <option value="transferred">Transferido</option>
-                </SelectComponent>
+                <div class="modal-content-div-error-div">
+                    <SelectComponent
+                        labelTitle="Nível de leitura"
+                        placeholderValue="Nível de leitura"
+                        icon="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l293.1 0c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1l-91.4 0zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"
+                        typeValue="select"
+                        :value="createExamData.reading"
+                        valueField="id"
+                        RightAction="display: none;"
+                        @input="createExamData.reading = $event.target.value"
+                    >
+                        <option value="not_reader">Não leitor</option>
+                        <option value="syllable_reader">Leitor de sílabas</option>
+                        <option value="word_reader">Leitor de palavras</option>
+                        <option value="sentence_reader">Leitor de frases</option>
+                        <option value="no_fluent_text_reader">Leitor de texto sem fluência</option>
+                        <option value="fluent_text_reader">Leitor de texto com fluência</option>
+                        <option value="missed">Faltou</option>
+                        <option value="transferred">Transferido</option>
+                    </SelectComponent>
+                    <div class="alert alert-danger" role="alert" v-if="hasErrors.reading">
+                        <span class="error-span font-medium">O campo nível de leitura é obrigatório</span>
+                    </div>
+                </div>
 
-                <SelectComponent
-                    labelTitle="Nível de escrita"
-                    placeholderValue="Nível de escrita"
-                    icon="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l293.1 0c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1l-91.4 0zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"
-                    typeValue="select"
-                    :value="createExamData.writing"
-                    valueField="id"
-                    RightAction="display: none;"
-                    @input="createExamData.writing = $event.target.value"
-                >
-                    <option value="pre_syllabic">Pré silábico</option>
-                    <option value="syllabic">Silábico</option>
-                    <option value="alphabetical_syllabic">Silábico alfabético</option>
-                    <option value="alphabetical">Alfabético</option>
-                    <option value="missed">Faltou</option>
-                    <option value="transferred">Transferido</option>
-                </SelectComponent>
+                <div class="modal-content-div-error-div">
+                    <SelectComponent
+                        labelTitle="Nível de escrita"
+                        placeholderValue="Nível de escrita"
+                        icon="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l293.1 0c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1l-91.4 0zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"
+                        typeValue="select"
+                        :value="createExamData.writing"
+                        valueField="id"
+                        RightAction="display: none;"
+                        @input="createExamData.writing = $event.target.value"
+                    >
+                        <option value="pre_syllabic">Pré silábico</option>
+                        <option value="syllabic">Silábico</option>
+                        <option value="alphabetical_syllabic">Silábico alfabético</option>
+                        <option value="alphabetical">Alfabético</option>
+                        <option value="missed">Faltou</option>
+                        <option value="transferred">Transferido</option>
+                    </SelectComponent>
+                    <div class="alert alert-danger" role="alert" v-if="hasErrors.writing">
+                        <span class="error-span font-medium">O campo nível de leitura é obrigatório</span>
+                    </div>
+                </div>
 
                 <div class="col-1" v-for="(literacyParameter, index) in literacyParameters" :key="index">
                     <h3>{{literacyParameterTranslator(literacyParameter.literacy_parameter)}}</h3>
@@ -534,6 +570,32 @@ const submitExamCreated = async () => {
 </template>
 
 <style scoped>
+
+.alert {
+    position: relative;
+    padding: 5px 20px;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    font-size: 16px;
+    line-height: 1.5;
+    display: flex;
+    align-items: center;
+}
+
+.modal-content-div-error-div {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 1rem;
+    width: 100%;
+}
+
+.error-span {
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+}
+
 .close-class {
     all: unset;
     cursor: pointer;
