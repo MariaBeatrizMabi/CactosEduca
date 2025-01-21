edac0884f0801e9571e8119877e08067a89b82e5
maurelio <marcos.aurelio@3esolucoes.com.br>
2025-01-21 00:07:31 -0300

    [FTR] - Inserindo tipo de turma

--- resources/js/pages/AdminManagement.vue
+++ resources/js/pages/AdminManagement.vue
@@ -101,6 +101,7 @@ const formDataStudentExport = ref({
 const formDataClassAdd = ref({
     name: "",
     shift: "",
+    type: "",
     school_id: userID.value,
     teacher_id: "",
 });
@@ -135,6 +136,7 @@ let formDataClassUpdated = ref({
     name: "",
     shift: "",
     school_id: userID.value,
+    type: "",
     teacher_id: "",
 });
 
@@ -178,6 +180,7 @@ function resetFormClass() {
         name: "",
         school_id: schoolId.value,
         teacher_id: "",
+        type: "",
         shift: '',
         year: ''
     };
@@ -298,6 +301,7 @@ async function submitStudentForm() {
 }
 
 async function submitFormClass() {
+
     try {
         isLoading.value = true;
         await axios.post("/ClassSchoolCreate", {
@@ -386,6 +390,7 @@ async function UpdateSchoolClassData(id) {
         name: data.name,
         shift: data.shift,
         year: data.year,
+        type: data.type,
         teacher_id: data.teacher_id,
     };
 }
@@ -417,6 +422,7 @@ async function updateClassDataForm() {
             name: formDataClassUpdated.value.name,
             shift: formDataClassUpdated.value.shift,
             year: formDataClassUpdated.value.year,
+            type: formDataClassUpdated.value.type,
             school_id: schoolId.value,
             teacher_id: formDataClassUpdated.value.teacher_id,
         };
@@ -934,6 +940,21 @@ async function handleImportStudents() {
                     />
 
                     <NewSelectComponent
+                        labelTitle="Tipo"
+                        placeholderValue="Tipo"
+                        icon="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l293.1 0c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1l-91.4 0zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"
+                        typeValue="select"
+                        :value="formDataClassAdd.type"
+                        valueField="id"
+                        RightAction="display: none;"
+                        @input="formDataClassAdd.type = $event.target.value"
+                    >
+                        <option value="preschool">Infantil</option>
+                        <option value="elementary_school">Fundamental 1</option>
+                        <option value="middle_school">Fundamental 2</option>
+                    </NewSelectComponent>
+
+                    <NewSelectComponent
                         labelTitle="Turno"
                         placeholderValue="Turno"
                         icon="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l293.1 0c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1l-91.4 0zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"
@@ -1017,6 +1038,22 @@ async function handleImportStudents() {
                                 $event.target.value
                         "
                     />
+
+                    <NewSelectComponent
+                        labelTitle="Tipo"
+                        placeholderValue="Tipo"
+                        icon="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l293.1 0c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1l-91.4 0zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z"
+                        typeValue="select"
+                        :value="formDataClassUpdated.type"
+                        valueField="id"
+                        RightAction="display: none;"
+                        @input="formDataClassUpdated.type = $event.target.value"
+                    >
+                        <option value="preschool">Infantil</option>
+                        <option value="elementary_school">Fundamental 1</option>
+                        <option value="middle_school">Fundamental 2</option>
+                    </NewSelectComponent>
+
                     <NewSelectComponent
                         labelTitle="Turno"
                         placeholderValue="Turno"
