88619143d93461d200562b23bffc39721a228c31
maurelio <marcos.aurelio@3esolucoes.com.br>
2025-01-20 23:14:45 -0300

    [FTR] - Adicionando tooltip na tela de visualização da turma

--- resources/js/components/tableComponent.vue
+++ resources/js/components/tableComponent.vue
@@ -55,6 +55,8 @@ const props = defineProps({
     }
 });
 
+const tooltipVisible = ref(false);
+
 const emits = defineEmits([
     'viewDetails',
     'updateAction',
@@ -64,6 +66,19 @@ const emits = defineEmits([
     'importData',
     'createExam'
 ]);
+
+const showTooltip = (elementId) => {
+    const element = document.getElementById(elementId)
+    element.classList.add('d-flex')
+    element.classList.remove('d-none')
+}
+
+const hideTooltip = (elementId) => {
+    const element = document.getElementById(elementId)
+    element.classList.remove('d-flex')
+    element.classList.add('d-none')
+}
+
 </script>
 
 <template>
@@ -126,23 +141,57 @@ const emits = defineEmits([
                             <td v-for="(cell, cellIndex) in Object.entries(row).filter(([key]) => key !== 'id')" :key="cellIndex">{{ cell[1] }}</td>
                             <td v-if="TableActions">
                                 <div class="actions">
-                                    <div class="show" @click="$emit('viewDetails', row.id)">
+                                    <div
+                                        @mouseover="showTooltip(`${rowIndex}-show-class`)"
+                                        @mouseleave="hideTooltip(`${rowIndex}-show-class`)"
+                                        class="show"
+                                        @click="$emit('viewDetails', row.id)">
                                         <svg width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                                             <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/>
                                         </svg>
+                                        <div class="tooltip d-none" :id="`${rowIndex}-show-class`">
+                                            Visualizar informações da turma
+                                        </div>
                                     </div>
-                                    <div class="intervention" v-if="InsertExam" @click="$emit('createExam', row.id)">
+                                    <div
+                                        @mouseover="showTooltip(`${rowIndex}-create-exam`)"
+                                        @mouseleave="hideTooltip(`${rowIndex}-create-exam`)"
+                                        class="intervention"
+                                        v-if="InsertExam"
+                                        @click="$emit('createExam', row.id)"
+                                    >
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M183.6 42.4C177.5 35.8 169 32 160 32s-17.5 3.8-23.6 10.4l-88 96c-11.9 13-11.1 33.3 2 45.2s33.3 11.1 45.2-2L128 146.3 128 448c0 17.7 14.3 32 32 32s32-14.3 32-32l0-301.7 32.4 35.4c11.9 13 32.2 13.9 45.2 2s13.9-32.2 2-45.2l-88-96zM320 320c0 17.7 14.3 32 32 32l50.7 0-73.4 73.4c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8l128 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-50.7 0 73.4-73.4c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8l-128 0c-17.7 0-32 14.3-32 32zM416 32c-12.1 0-23.2 6.8-28.6 17.7l-64 128-16 32c-7.9 15.8-1.5 35 14.3 42.9s35 1.5 42.9-14.3l7.2-14.3 88.4 0 7.2 14.3c7.9 15.8 27.1 22.2 42.9 14.3s22.2-27.1 14.3-42.9l-16-32-64-128C439.2 38.8 428.1 32 416 32zM395.8 176L416 135.6 436.2 176l-40.4 0z"/></svg>
+                                        <div class="tooltip d-none" :id="`${rowIndex}-create-exam`">
+                                            Cadastrar sondagem
+                                        </div>
                                     </div>
-                                    <div class="edit" v-if="TableActionUpdate" @click="$emit('updateAction', row.id)">
+                                    <div
+                                        class="edit"
+                                        @mouseover="showTooltip(`${rowIndex}-update-class`)"
+                                        @mouseleave="hideTooltip(`${rowIndex}-update-class`)"
+                                        v-if="TableActionUpdate"
+                                        @click="$emit('updateAction', row.id)"
+                                    >
                                         <svg width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                             <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
                                         </svg>
+                                        <div class="tooltip d-none" :id="`${rowIndex}-update-class`">
+                                            Atualizar turma
+                                        </div>
                                     </div>
-                                    <div class="deleted" v-if="TableActionDelete" @click="$emit('deletedAction', row.id)">
+                                    <div
+                                        class="deleted"
+                                        v-if="TableActionDelete"
+                                        @click="$emit('deletedAction', row.id)"
+                                        @mouseover="showTooltip(`${rowIndex}-delete-class`)"
+                                        @mouseleave="hideTooltip(`${rowIndex}-delete-class`)"
+                                    >
                                         <svg width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                             <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
                                         </svg>
+                                        <div class="tooltip-delete d-none" :id="`${rowIndex}-delete-class`">
+                                            Deletar turma
+                                        </div>
                                     </div>
                                 </div>
                             </td>
@@ -181,5 +230,39 @@ const emits = defineEmits([
     & > td:last-child, th:last-child {
         border-right: 0;
     }
+}
+
+.tooltip {
+    position: absolute;
+    margin-top: -50px;
+    background-color: #333;
+    color: #fff;
+    padding: 5px 10px;
+    border-radius: 4px;
+    font-size: 12px;
+    white-space: nowrap;
+    z-index: 1000;
+    opacity: 0.9;
+}
+
+.tooltip-delete {
+    position: absolute;
+    margin-top: -120px;
+    background-color: #333;
+    color: #fff;
+    padding: 5px 10px;
+    border-radius: 4px;
+    font-size: 12px;
+    white-space: nowrap;
+    z-index: 1000;
+    opacity: 0.9;
+}
+
+.d-none {
+    display: none;
+}
+
+.d-flex{
+    display: flex;
 }
 </style>
