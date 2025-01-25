6f3bddb94494c85beed8c584c64476781715e539
maurelio <marcos.aurelio@3esolucoes.com.br>
2025-01-25 15:09:32 -0300

    [FIX] - Corrigindo nomenclaturas da tela de turma

--- resources/js/pages/ClassDetails.vue
+++ resources/js/pages/ClassDetails.vue
@@ -23,7 +23,7 @@ const actualHost = ref('');
 const showAddStudentModal = ref(false);
 const showRemoveStudentModal = ref(false);
 const openedExportDocumentModal = ref(false);
-const selectedBimonthly = ref('');
+const selectedExam = ref('');
 
 const studentIdToRemove = ref(null);
 
@@ -290,7 +290,7 @@ const submitExamCreated = async () => {
 
 const openExportDocumentModal = () => {
     openedExportDocumentModal.value = true
-    selectedBimonthly.value = ''
+    selectedExam.value = ''
 }
 
 </script>
@@ -451,18 +451,18 @@ const openExportDocumentModal = () => {
         <div class="modal-body-size">
             <div class="modal-content-div">
                 <div class="input-component">
-                    <label>Bimestre desejado</label>
+                    <label>Sondagem</label>
                     <div class="input-formating">
                         <svg width="9" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                             <path d="M337.8 5.4C327-1.8 313-1.8 302.2 5.4L166.3 96H48C21.5 96 0 117.5 0 144V464c0 26.5 21.5 48 48 48H256V416c0-35.3 28.7-64 64-64s64 28.7 64 64v96H592c26.5 0 48-21.5 48-48V144c0-26.5-21.5-48-48-48H473.7L337.8 5.4zM96 192h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H96c-8.8 0-16-7.2-16-16V208c0-8.8 7.2-16 16-16zm400 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H512c-8.8 0-16-7.2-16-16V208zM96 320h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H96c-8.8 0-16-7.2-16-16V336c0-8.8 7.2-16 16-16zm400 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v64c0 8.8-7.2 16-16 16H512c-8.8 0-16-7.2-16-16V336zM232 176a88 88 0 1 1 176 0 88 88 0 1 1 -176 0zm88-48c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H336V144c0-8.8-7.2-16-16-16z"/>
                         </svg>
                         <hr>
-                        <select v-model="selectedBimonthly">
-                            <option value="" disabled selected>Selecione um bimestre</option>
-                            <option value=1>1° Bimestre</option>
-                            <option value=2>2° Bimestre</option>
-                            <option value=3>3° Bimestre</option>
-                            <option value=4>4° Bimestre</option>
+                        <select v-model="selectedExam">
+                            <option value="" disabled selected>Selecione uma sondagem</option>
+                            <option value=1>1° Sondagem</option>
+                            <option value=2>2° Sondagem</option>
+                            <option value=3>3° Sondagem</option>
+                            <option value=4>4° Sondagem</option>
                         </select>
                     </div>
                 </div>
@@ -476,8 +476,8 @@ const openExportDocumentModal = () => {
                 Fechar
             </a>
             <a class="school-add"
-               :class="{ disabled: selectedBimonthly === '' }"
-               :href="`${actualHost}/literacy-parameters/export-document/${classData.id}/${selectedBimonthly}`"
+               :class="{ disabled: selectedExam === '' }"
+               :href="`${actualHost}/literacy-parameters/export-document/${classData.id}/${selectedExam}`"
                download=""
             >
                 <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
